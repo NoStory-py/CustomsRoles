69bf9d88fb61ff7f4b5f0d5afadae3a75d22685e
NoStory-py <nostoryatall@gmail.com>
2024-08-24 00:29:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import discord
 from discord import app_commands
 import os
 
-TOKEN = os.getenv('BOT_TOKEN')
+TOKEN = os.getenv("BOT_TOKEN")
 
 intents = discord.Intents.default()
-intents.members = True  # Enable member intents
+intents.members = True 
 bot = discord.Client(intents=intents)
 tree = app_commands.CommandTree(bot)
 
@@ -19,62 +19,61 @@ async def customs_assign(interaction: discord.Interaction):
 
     await interaction.response.defer()
 
-    role_to_check1 = next((x for x in interaction.guild.roles if "Red: " in x.name), None)
-    role_to_give1 = discord.utils.get(interaction.guild.roles, name='Team Magma')
+    check_red = None
+    team_magma = discord.utils.get(interaction.guild.roles, name='Team Magma')
+    check_blue = None
+    team_aqua = discord.utils.get(interaction.guild.roles, name='Team Aqua')
+    magma_added = []
+    aqua_added = []
 
-    listm = []
+    for x in interaction.guild.roles:
+        if "Red: " in x.name:
+            check_red = x
+        elif "Blue: " in x.name:
+            check_blue = x
+    
+    if check_blue is None and check_red is None:
+        return await interaction.followup.send("Failed Coudnt find the Role Red: and Blue: , Lobby isnt created yet.")
 
-    for member in interaction.guild.members:  
-        if role_to_check1 in member.roles:
-            listm.append(member.name)  
-            await member.add_roles(role_to_give1) 
+    if check_red is not None:
+        for member in check_red.members:  
+            await member.add_roles(team_magma) 
+            magma_added.append(member.nick if member.nick else member.name)
 
-    role_to_check2 = next((x for x in interaction.guild.roles if "Blue: " in x.name), None)
-    role_to_give2 = discord.utils.get(interaction.guild.roles, name='Team Aqua')
+    if check_blue is not None:   
+        for member in check_blue.members:  
+            await member.add_roles(team_aqua)
+            aqua_added.append(member.nick if member.nick else member.name)
 
-    lista = []
-
-    for member in interaction.guild.members:  
-        if role_to_check2 in member.roles:  
-            lista.append(member.name)
-            await member.add_roles(role_to_give2)
-            
-    magma = bot.get_channel(1175476709999526048)
-    aqua = bot.get_channel(1175476817440804974)
-
-    if magma is not None:
-        await magma.send(f"{role_to_give1.mention} Private team chat!")
-    if aqua is not None:
-        await aqua.send(f"{role_to_give2.mention} Private team chat!")
-
-    await interaction.followup.send(f"Assigned Magma to: {listm}\nAssigned Aqua to: {lista}")
-
+    # main magma_chat = bot.get_channel(1175476709999526048)
+    # main aqua_chat = bot.get_channel(1175476817440804974)
+    magma_chat = bot.get_channel(1273275120391164007)
+    aqua_chat = bot.get_channel(1273275140620156950)
+    '''
+    if magma_chat is not None:
+        await magma_chat.send(f"{team_magma.mention} Private team chat!")
+    if aqua_chat is not None:
+        await aqua_chat.send(f"{team_aqua.mention} Private team chat!")
+    '''
+    return await interaction.followup.send(f"Assigned Magma to: {", ".join(magma_added)}\nAssigned Aqua to: {", ".join(aqua_added)}")
 
 @tree.command(name="customs_remove", description="Remove team roles")
 async def customs_remove(interaction: discord.Interaction):
 
     await interaction.response.defer()
 
-    role_to_check1 = discord.utils.get(interaction.guild.roles, name='Team Magma')
-    
-    listm = []
+    team_magma = discord.utils.get(interaction.guild.roles, name='Team Magma')
+    team_aqua = discord.utils.get(interaction.guild.roles, name='Team Aqua')
+    magma_removed = []
+    aqua_removed = []
 
-    for member in interaction.guild.members:  
-        
-        if role_to_check1 in member.roles:  
-            listm.append(member.name)
-            await member.remove_roles(role_to_check1) 
-
-    role_to_check2 = discord.utils.get(interaction.guild.roles, name='Team Aqua')
-    
-    lista = []
-
-    for member in interaction.guild.members:  
-        
-        if role_to_check2 in member.roles:  
-            lista.append(member.name)
-            await member.remove_roles(role_to_check2)
-    
-    await interaction.followup.send(f"Removed Magma from: {listm}\nRemoved Aqua from: {lista}")
+    for member in team_magma.members:  
+        await member.remove_roles(team_magma) 
+        magma_removed.append(member.nick if member.nick else member.name)
+    for member in team_aqua.members:  
+        await member.remove_roles(team_aqua) 
+        aqua_removed.append(member.nick if member.nick else member.name)
+            
+    return await interaction.followup.send(f"Removed Magma from: {", ".join(magma_removed)}\nRemoved Aqua from: {", ".join(aqua_removed)}")
 
 bot.run(TOKEN)
